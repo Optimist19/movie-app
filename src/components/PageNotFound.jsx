cb22f12df45674a647b8c0390e989047974e2e0e
optimist19 <oluwasegunbamidele3@gmail.com>
2024-05-06 10:04:53 +0100

    perfected the 404 and the tvdetail image(error like message.

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -7,18 +7,23 @@ function PageNotFound() {
   // console.error(error);
 
   const style = {
-    backgroundImage: `url(${pageNotFound})`,
+    backgroundImage: `url(${pageNotFound})`
   };
 
   return (
     <section>
-      <div className="h-[100vh] relative" style={style}>
+      <div className=" relative">
         <Link to="/" className="absolute top-4 right-6">
           <div className="flex items-center gap-2 text-xl sm:text-2xl">
             <MdArrowBackIosNew />
             <p>Back Home</p>
           </div>
         </Link>
+      </div>
+      <div className="flex flex-col items-center justify-center h-[100vh]">
+        <div className="w-[100%]">
+          <img src={pageNotFound} alt="" className="w-[100%]"/>
+        </div>
       </div>
     </section>
   );
--- src/components/TvDetails.jsx
+++ src/components/TvDetails.jsx
@@ -1,24 +1,24 @@
 import { CiWarning } from "react-icons/ci";
 
-import technicalDifficulties from "../assets/sportsmanias-technical-difficulties.gif";
+import technicalDifficulties from "../assets/360_F_471346449_EPhL2CNDHlb1fJH4VxOm9qJ33esvkfqw.jpg";
 
 function TvDetails() {
-  const style = {
-    backgroundImage: `url(${technicalDifficulties})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center"
-  };
+
   
   return (
     <section className="">
       <div
-        className=" h-[100vh]"
-        style={style}>
-        <div className="flex flex-col justify-center items-center gap-3 pt-[12vh]">
+        className="flex flex-col justify-center items-center gap-3 pt-[12vh] h-[100vh]"
+        >
+        <div className="flex flex-col items-center">
           <h1 className="sm:text-3xl text-center ">
             Kindly check back tomorrow, we have an error.
           </h1>
           <CiWarning className="text-[#f43f5e] sm:text-2xl" />
+          <div className="w-[30vw] mt-4">
+
+          <img src={technicalDifficulties} alt="" />
+          </div>
         </div>
       </div>
     </section>
